5295469997301c8696c7a45c2ba8780aa0182aec
Gamattowicz <p.romanczuk31@gmail.com>
2024-03-11 13:06:55 +0100

    Add ThemeProvider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,33 +1,44 @@
-import type { Metadata } from "next";
-import { Roboto_Slab } from "next/font/google";
 import "./globals.css";
-
+import { Roboto_Slab, Lato } from "next/font/google";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { cx } from "./utils";
+import { ThemeProvider } from "./components/theme-provider";
 
 const roboto_slab = Roboto_Slab({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-slab",
 });
 
-export const metadata: Metadata = {
-  title: "E-commerce",
-  description: "E-commerce App created by Gamattowicz",
-};
+const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lato",
+  weight: "400",
+});
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" data-theme="night">
-        <body className={roboto_slab.className}>
+    <html lang="en">
+      <body className={cx(roboto_slab.variable, lato.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="flex flex-col min-h-screen max-h-full">
             <Header />
-            <main className="flex-1 grid place-items-center">{children}</main>
+            <main>{children}</main>
             <Footer />
           </div>
-        </body>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
