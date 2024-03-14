3b5b2d8aab32b786377e69b1de301fc855289141
Gamattowicz <p.romanczuk31@gmail.com>
2024-03-14 19:16:37 +0100

    Add LoginForm

---
+++ frontend/src/app/components/LoginForm.tsx
@@ -0,0 +1,124 @@
+"use client";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+import { PasswordInput } from "@/components/ui/password-input";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const formData = new FormData();
+      for (const key in values) {
+        formData.append(key, values[key as keyof typeof values]);
+      }
+
+      const res = await fetch("http://127.0.0.1:8000/user/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        console.error("Server response:", errorData);
+        throw new Error(errorData.detail);
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error: any) {
+      console.error("Error fetching products:", error.message);
+    }
+  };
+  return (
+    <>
+      <Card className="w-full max-w-sm">
+        <CardHeader className="text-center">
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Email address"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder="••••••••" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>Don&apos;t have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
