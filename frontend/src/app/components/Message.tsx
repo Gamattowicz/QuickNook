d7e204f717343fa6d87a5e43932d7545a510baba
Gamattowicz <p.romanczuk31@gmail.com>
2024-03-16 12:54:58 +0100

    Add Message component

---
+++ frontend/src/app/components/Message.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { CircleAlert, CircleCheck, CircleHelp } from "lucide-react";
+
+interface MessageProps {
+  variant:
+    | "default"
+    | "primary"
+    | "destructive"
+    | "accent"
+    | "muted"
+    | "secondary";
+  title: string;
+  description: string;
+}
+export default function Message({ variant, title, description }: MessageProps) {
+  const Icon =
+    variant === "destructive"
+      ? CircleAlert
+      : variant === "muted"
+      ? CircleCheck
+      : CircleHelp;
+
+  return (
+    <div className="w-full max-w-sm">
+      <Alert variant={variant}>
+        <Icon className="h-5 w-5" />
+        <AlertTitle>{title}</AlertTitle>
+        <AlertDescription>{description}</AlertDescription>
+      </Alert>
+    </div>
+  );
+}
---
+++ frontend/src/components/ui/alert.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const alertVariants = cva(
+  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-33px] [&>svg]:absolute [&>svg]:left-3 [&>svg]:bottom-6 [&>svg]:text-foreground [&>svg~*]:pl-7",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        primary:
+          "bg-primary text-primary-foreground border-primary-foreground/50 dark:border-primary-foreground [&>svg]:text-primary-foreground",
+        destructive:
+          "bg-destructive text-destructive-foreground border-destructive-foreground/50 dark:border-destructive-foreground [&>svg]:text-destructive-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground border-secondary-foreground/50 dark:border-secondary-foreground [&>svg]:text-secondary-foreground",
+        accent:
+          "bg-accent text-accent-foreground border-accent-foreground/50 dark:border-accent-foreground [&>svg]:text-accent-foreground",
+        muted:
+          "bg-muted text-muted-foreground border-muted-foreground/50 dark:border-muted-foreground [&>svg]:text-muted-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+const Alert = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+));
+Alert.displayName = "Alert";
+
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+));
+AlertTitle.displayName = "AlertTitle";
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+));
+AlertDescription.displayName = "AlertDescription";
+
+export { Alert, AlertTitle, AlertDescription };
