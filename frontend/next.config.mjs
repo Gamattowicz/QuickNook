d4e44cb8e028e742acf9cb886f153475874a52cd
Gamattowicz <p.romanczuk31@gmail.com>
2024-03-25 13:39:13 +0100

    Add remotePatterns to images in next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/api/images/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/api/thumbnails/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
