81db81e7763b262059fffe37f5082a5e48648688
Gamattowicz <p.romanczuk31@gmail.com>
2024-04-19 14:47:04 +0200

    Fix filtering functionality

--- api/utils/filtering_helpers.py
+++ api/utils/filtering_helpers.py
@@ -10,15 +10,14 @@ logger = logging.getLogger(__name__)
 def apply_filters(
     filters: dict[str, Any], query_source: Union[Table, Select]
 ) -> Select:
+    query = query_source.select()
     if isinstance(query_source, Table):
         logger.info(f"Getting all {query_source.name} with filters")
-        query = query_source.select()
     elif isinstance(query_source, Select):
         logger.info("Applying filters to Select query")
-        query = query_source
     filters_kv_pairs = {}
     for key, value in filters.items():
-        if value is not None:
+        if value:
             column = getattr(query_source.c, key)
             filters_kv_pairs[key] = value
             query = query.where(func.lower(column) == func.lower(value))
